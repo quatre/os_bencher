dee597ca472de1fff2329f1141e9c0c7c0b3fbed
Guillaume Espanel <guillaume.espanel@objectif-libre.com>
2016-10-19 19:48:11 +0200

    Add the files

---
+++ sysbench.sh
@@ -0,0 +1,121 @@
+#! /bin/bash
+apt-get update; apt-get install sysbench mysql-server -y -q
+mysql -u root -e 'create database test;';
+
+
+
+RESULT_DIR=$(mktemp -d)
+CPU_COUNT=$(cat /proc/cpuinfo  | grep "^processor" | wc -l)
+
+MAX_PRIMES=20000
+TEST_TIMEOUT=30
+OLTP_ROWS_NUM=1000000
+FILE_SIZE=40G
+
+MAX_PRIMES=2000
+TEST_TIMEOUT=3
+OLTP_ROWS_NUM=1000
+FILE_SIZE=40M
+
+sysbench --test=oltp --oltp-table-size=$OLTP_ROWS_NUM --mysql-db=test --mysql-user=root prepare
+
+
+function cpu_prime {
+    THREAD_COUNT=$1
+    RESULT_FILE="$RESULT_DIR/${FUNCNAME[0]}.${THREAD_COUNT}.txt"
+
+    COMMAND="sysbench --max-time=$TEST_TIMEOUT --cpu-max-prime=$MAX_PRIMES --test=cpu --num-threads=$THREAD_COUNT run"
+    echo $COMMAND
+    echo "# $COMMAND" > $RESULT_FILE
+    echo "# ${FUNCNAME[0]} THREAD_COUNT=$1 MAX_PRIMES=$MAX_PRIMES" >> $RESULT_FILE
+    $COMMAND >> $RESULT_FILE
+}
+
+
+function oltp {
+    THREAD_COUNT=$1
+    RESULT_FILE="$RESULT_DIR/${FUNCNAME[0]}.${THREAD_COUNT}.txt"
+   
+    COMMAND="sysbench --test=oltp --oltp-table-size=$OLTP_ROWS_NUM --mysql-db=test --mysql-user=root --num-threads=$THREAD_COUNT run"
+    echo $COMMAND
+    echo "# $COMMAND" > $RESULT_FILE
+    echo "# ${FUNCNAME[0]} THREAD_COUNT=$1 OLTP_ROW_NUM=$OLTP_ROWS_NUM" >> $RESULT_FILE
+    $COMMAND >> $RESULT_FILE
+}
+
+
+function io_rndrd {
+    THREAD_COUNT=$1
+    BLOCK_SIZE=$2
+    TEST_DIR=$3
+    RESULT_FILE="$RESULT_DIR/${FUNCNAME[0]}.${TEST_DIR}.${BLOCK_SIZE}.${THREAD_COUNT}.txt"
+
+    COMMAND="sysbench --test=fileio --file-total-size=$FILE_SIZE --file-test-mode=rndrd --max-time=$TEST_TIMEOUT --max-requests=0 --file-block-size=$BLOCK_SIZE --num-threads=$THREAD_COUNT run"
+    echo $COMMAND
+    echo "# $COMMAND" > $RESULT_FILE
+    echo "# ${FUNCNAME[0]}-${TEST_DIR} THREAD_COUNT=$1 BLOCK_SIZE=$2 TEST_DIR=$3" >> $RESULT_FILE
+    $COMMAND >> $RESULT_FILE
+}
+
+function io_rndwr {
+    THREAD_COUNT=$1
+    BLOCK_SIZE=$2
+    TEST_DIR=$3
+    RESULT_FILE="$RESULT_DIR/${FUNCNAME[0]}.${TEST_DIR}.${BLOCK_SIZE}.${THREAD_COUNT}.txt"
+
+    COMMAND="sysbench --test=fileio --file-total-size=$FILE_SIZE --file-test-mode=rndwr --max-time=$TEST_TIMEOUT --max-requests=0 --file-block-size=$BLOCK_SIZE --num-threads=$THREAD_COUNT run"
+    echo $COMMAND
+    echo "# $COMMAND" > $RESULT_FILE
+    echo "# ${FUNCNAME[0]}-${TEST_DIR} THREAD_COUNT=$1 BLOCK_SIZE=$2 TEST_DIR=$3" >> $RESULT_FILE
+    $COMMAND >> $RESULT_FILE
+}
+
+function io_seqwr {
+    THREAD_COUNT=$1
+    BLOCK_SIZE=$2
+    TEST_DIR=$3
+    RESULT_FILE="$RESULT_DIR/${FUNCNAME[0]}.${TEST_DIR}.${BLOCK_SIZE}.${THREAD_COUNT}.txt"
+
+    COMMAND="sysbench --test=fileio --file-total-size=$FILE_SIZE --file-test-mode=seqwr --max-time=$TEST_TIMEOUT --max-requests=0 --file-block-size=$BLOCK_SIZE --num-threads=$THREAD_COUNT run"
+    echo $COMMAND
+    echo "# $COMMAND" > $RESULT_FILE
+    echo "# ${FUNCNAME[0]}-${TEST_DIR} THREAD_COUNT=$1 BLOCK_SIZE=$2 TEST_DIR=$3" >> $RESULT_FILE
+    $COMMAND >> $RESULT_FILE
+}
+
+function io_seqrd {
+    THREAD_COUNT=$1
+    BLOCK_SIZE=$2
+    TEST_DIR=$3
+    RESULT_FILE="$RESULT_DIR/${FUNCNAME[0]}.${TEST_DIR}.${BLOCK_SIZE}.${THREAD_COUNT}.txt"
+
+    COMMAND="sysbench --test=fileio --file-total-size=$FILE_SIZE --file-test-mode=seqrd --max-time=$TEST_TIMEOUT --max-requests=0 --file-block-size=$BLOCK_SIZE --num-threads=$THREAD_COUNT run"
+    echo $COMMAND
+    echo "# $COMMAND" > $RESULT_FILE
+    echo "# ${FUNCNAME[0]}-${TEST_DIR} THREAD_COUNT=$1 BLOCK_SIZE=$2 TEST_DIR=$3" >> $RESULT_FILE
+    $COMMAND >> $RESULT_FILE
+}
+
+
+dest=$HOME/sb_files
+    mkdir -p $dest
+    pushd $dest
+    sysbench --test=fileio --file-total-size=$FILE_SIZE prepare
+
+    for tc in 1 2
+    do
+    	cpu_prime $tc
+    	oltp $tc
+    	for bs in 1K 4K 8K 64K
+    	do
+    		io_rndrd $tc $bs root
+    		io_rndwr $tc $bs root 
+    		io_seqrd $tc $bs root 
+    		io_seqwr $tc $bs root
+    	done
+    done
+
+    popd
+    rm -rf $dest
+
+python sysbench_parse.py $RESULT_DIR > sb_result.json
---
+++ sysbench_parse.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import os
+from os.path import join, getsize
+import json
+import unicodedata
+import re
+import sys
+
+
+def strip_and_count(line, level=0):
+    if line.startswith("    "):
+        level += 1
+        return strip_and_count(line[4:], level)
+    else:
+        return line, level
+
+
+def slugify(value):
+    """
+    Converts to lowercase, removes non-word characters (alphanumerics and
+    underscores) and converts spaces to hyphens. Also strips leading and
+    trailing whitespace.
+    """
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    return re.sub('[-\s]+', '-', value)
+
+
+def access_item(dictionnary, stack):
+    if len(stack) == 1:
+        return dictionnary.setdefault(stack[0], {})
+    elif len(stack) > 1:
+        return access_item(dictionnary[stack.pop(0)], stack)
+    else:
+        return dictionnary
+
+
+
+def parse_sysbench_result(path): # noqa
+    lines = open(path)
+    out = {}
+
+    out['test-cmd'] = re.match(r'# (.*)', lines.next()).group(1)
+    test_params = re.match(r'# (.*)', lines.next()).group(1).split(" ")
+    out['test-params'] = " ".join(test_params[1:])
+    out['test-name'] = slugify("-".join(test_params))
+    sys_bench_version = re.match(r'(sysbench .*):', lines.next()).group(1)
+    out['version'] = sys_bench_version
+
+    last_level = 0
+    last_key = None
+    stack = []
+    for line in lines:
+        parsed_line = re.match(r'Doing (.*)', line)
+        if parsed_line:
+            out["test"] = slugify(parsed_line.group(1))
+            continue
+        line, level = strip_and_count(line)
+
+        # Trying to get rw information
+        parsed_line = re.match(
+            r'Read (\S+)  Written (\S+)  Total transferred (\S+)  \((.*)\)',
+            line
+        )
+        if parsed_line:
+            out['transfered'] = {
+                'read': parsed_line.group(1),
+                'written': parsed_line.group(2),
+                'total': parsed_line.group(3),
+                'bandwidth': parsed_line.group(4),
+            }
+            continue
+
+        parsed_line = re.match(r'(.*):(\s+(.*))?$', line)
+        if parsed_line is None:
+            continue
+        key = slugify(parsed_line.group(1))
+
+        if level > last_level:
+            stack.append(last_key)
+        if level < last_level:
+            for i in range(level, last_level):
+                stack.pop()
+        current_pos = access_item(out, stack[:])
+        try:
+            value = parsed_line.group(3)
+            if value == '':
+                current_pos[key] = {}
+            else:
+                # Value may have additional option
+                parsed_value = re.match(r'(\S+)(\s+\(([^)]+)\)?)', value)
+                if parsed_value:
+                    current_pos[key] = parsed_value.group(1)
+                    current_pos[key+"-extra-data"] = parsed_value.group(3)
+                else:
+                    current_pos[key] = value
+        except:
+
+        last_level = level
+        last_key = key
+
+    # Post
+
+    if out['test'] in ("random-write-test",):
+        parsed_ops = re.match(r'(\d+) Read, (\d+) Write, (\d+) Other',
+                              out['operations-performed'])
+        out['operations-performed'] = {
+            'read': int(parsed_ops.group(1)),
+            'write': int(parsed_ops.group(2)),
+            'other': int(parsed_ops.group(3)),
+        }
+    return out
+
+
+benchmark_results = {}
+
+for root, dirs, files in os.walk(sys.argv[1]):
+    for name in files:
+        bench_res = parse_sysbench_result(join(root, name))
+        benchmark_results[bench_res['test-name']] = bench_res
+
+print json.dumps(benchmark_results, sort_keys=True,
+                 indent=4, separators=(',', ': '))
